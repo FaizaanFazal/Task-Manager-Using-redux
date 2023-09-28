310ec4667bcafe470ab6f4c579637664a05ec92e
Faizaan <dkfaizaan12@gmail.com>
2023-09-28 20:59:22 +0500

    msw error handling

--- src/__test__/User.test.js
+++ src/__test__/User.test.js
@@ -7,23 +7,24 @@ import { renderWithProviders }  from './todotesting';
 import { screen,fireEvent, render } from '@testing-library/react';
 import { expect } from '@jest/globals';
 import { User } from '../features/taskManager/User';
+import {server} from '../__mocks__/server'
+import { rest } from 'msw';
 
 
 
 test('renders  users', async () => { 
   render(<User/>)
-  const useritem=await screen.findAllByRole('itemlist')
-
- 
+  const useritem=await screen.findAllByRole('itemlist') 
   expect(useritem).toHaveLength(1);
  })
 
-// test('GetsUsers', async () => { 
-// renderWithProviders(<App/>);
-// //creating mock
-// const userMock =  { get:jest.fn() }
-// const sut =  getUsers(userMock);
-// sut.getUsers();
-// expect(userMock.get).mockResolvedValue("faizaan")
-
-// })
+test('renders error', async ()=>{
+  server.use(
+    rest.get("https://jsonplaceholder.typicode.com/users",(req,res,ctx)=>{
+      return res(ctx.status(500));
+    })
+  )
+  render(<User/>)
+  const error = await screen.findByText("Error fetching users")
+  expect(error).toBeInTheDocument();
+})
